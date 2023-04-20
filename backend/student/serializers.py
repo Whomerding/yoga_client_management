a9fd5ab9f1bff768d446967ec8289b03efb2eb48
Whomerding <wendy.homerding@gmail.com>
2023-04-20 09:50:51 -0400

    added fields to the student serializer model, created student views, created student URLS

--- backend/student/serializers.py
+++ backend/student/serializers.py
@@ -4,5 +4,5 @@ from .models import Student
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
-        fields = ['first_name', 'last_name', 'address', 'phone_number', 'email', 'studio']
+        fields = ['first_name', 'last_name', 'address', 'phone_number', 'email', 'current_class_package','classes_remaining', 'studio']
         depth = 1 
--- backend/student/views.py
+++ backend/student/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import StudentSerializer
+from .models import Student
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def student(request):
+    if request.method == 'POST':
+        Student = Student.objects.all ()
+        serializer = StudentSerializer(data=request.data)
+        serializer.is_valid (raise_exception=True)
+        serializer.save ()
+        return Response (serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view (['GET', 'PUT', 'DELETE'])
+@permission_classes([IsAuthenticated])
+def student_detail(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    if request.method == 'GET': 
+        serializer = StudentSerializer(student)
+        return Response (serializer.data)
+    elif request.method == 'PUT':  
+        serializer = StudentSerializer(student, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        student.delete()
+        return Response (status=status.HTTP_204_NO_CONTENT)
