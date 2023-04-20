23fb56e3449673f43283eab2d6eae7cec55325aa
Whomerding <wendy.homerding@gmail.com>
2023-04-20 09:03:49 -0400

    added two fields on the student model, current_class_package and classes_remaining both nullable

--- backend/student/models.py
+++ backend/student/models.py
@@ -6,4 +6,6 @@ class Student(models.Model):
     address = models.CharField (max_length=500)
     phone_number = models.IntegerField()
     email = models.EmailField(max_length=255)
+    current_class_package = models.CharField (max_length=255, null=True)
+    classes_remaining = models.IntegerField(null=True)
     studio = models.ForeignKey(Studio, on_delete=models.CASCADE)
