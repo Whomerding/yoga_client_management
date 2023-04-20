9e557a7b359e2dc9ef61e4dd6ac396adbde7d8be
Whomerding <wendy.homerding@gmail.com>
2023-04-20 09:57:24 -0400

    finished classes_taken views+classes_taken urls

--- backend/classes_taken/views.py
+++ backend/classes_taken/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ClassesTakenSerializer
+from .models import ClassesTaken
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def class_log(request):
+    if request.method == "GET":
+        classes_taken = ClassesTaken.objects.all()
+        serializer = ClassesTakenSerializer(classes_taken, many = True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ClassesTakenSerializer(data=request.data)
+        serializer.is_valid (raise_exception=True)
+        serializer.save ()
+        return Response (serializer.data, status=status.HTTP_201_CREATED)
