12f38952809ba0d11223598ec0ec6d1af52b3c52
Whomerding <wendy.homerding@gmail.com>
2023-04-20 09:20:55 -0400

    created views for studio app

--- backend/studio/views.py
+++ backend/studio/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import StudioSerializer
+from .models import Studio
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def create_studio(request):
+    if request.method == 'POST':
+        studio = Studio.objects.all ()
+        serializer = StudioSerializer(data=request.data)
+        serializer.is_valid (raise_exception=True)
+        serializer.save ()
+        return Response (serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view (['GET', 'PUT', 'DELETE'])
+@permission_classes([IsAuthenticated])
+def studio_detail(request, pk):
+    studio = get_object_or_404(Studio, pk=pk)
+    if request.method == 'GET': 
+        serializer = StudioSerializer(studio)
+        return Response (serializer.data)
+    elif request.method == 'PUT':  
+        serializer = StudioSerializer(studio, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        studio.delete()
+        return Response (status=status.HTTP_204_NO_CONTENT)
