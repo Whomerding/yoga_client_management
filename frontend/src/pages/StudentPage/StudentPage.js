d4b0b0fe09ada982f6565652a4c57829834cecbd
Whomerding <wendy.homerding@gmail.com>
2023-05-02 15:19:21 -0400

    working on fixing conditional rendering issue on student page

--- frontend/src/pages/StudentPage/StudentPage.js
+++ frontend/src/pages/StudentPage/StudentPage.js
@@ -9,47 +9,45 @@ import ClassList from '../../components/ClassList/ClassList';
 const StudentPage = () => {
     const [user, token] = useAuth ();
     const [studioPackages, setStudioPackages]=useState([]);
-    const [student, setStudent]=useState([]);
+    const [student, setStudent]=useState(
+        
+    );
 
     useEffect(()=> {
-      getAllClassPackages();
       getStudents();
+      getAllClassPackages();
     }, []);
     
+    async function getStudents() {      
+      try {
+        const response = await axios.get(`http://127.0.0.1:8000/api/student/`)
+        const filteredStudent = response.data.filter(el=>(el.email.toLowerCase()===(user.email.toLowerCase())));
+        setStudent(filteredStudent[0]);
+                 
+      } catch (error) {
+        console.log (error)
+      }}
+
     async function getAllClassPackages() {
       const response = await axios.get(`http://127.0.0.1:8000/api/classpackage/`, {headers: {Authorization:"Bearer " + token}});
       setStudioPackages(response.data);
   }
 
-  async function getStudents() {      
-    try {
-      const response = await axios.get(`http://127.0.0.1:8000/api/student/`)
-      const filteredStudent = response.data.filter(el=>(el.email.toLowerCase()===(user.email.toLowerCase())));
-      setStudent(filteredStudent[0]);
-               
-    } catch (error) {
-        console.log(error)
-    }}
-    
-    
-
-    console.log(student)
+    console.log(student.id)
     console.log(studioPackages)
-
 
     return ( 
         <div>
             <div>
-                {student.id ? (<StudentInfoDisplay student={student}/>): (<StudentForm/>)}
+              {student.id ? (<StudentInfoDisplay student={student}/>) : (<StudentForm/>)}
+            </div> 
+            <div>
+              <CheckInButton student={student}/>
             </div>
             <div>
-                <CheckInButton student={student}/>
-            </div>
-            <div>
-                <ClassList student={student} studioPackages={studioPackages}/>
+              <ClassList student={student} studioPackages={studioPackages}/>
             </div>
         </div>
-
      );
 }
  
